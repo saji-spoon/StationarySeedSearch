767a9e736672631119b11c219d2783422d6fb272
saji-spoon-win8-msys2 <tkgkmny0707@hotmail.co.jp>
2017-03-07 23:31:23 +0900

    Creating DB with GUI is Available.

--- StationarySeedSearch/stationaryseedsearch.cpp
+++ StationarySeedSearch/stationaryseedsearch.cpp
@@ -1,11 +1,80 @@
 #include "stationaryseedsearch.h"
 
 StationarySeedSearch::StationarySeedSearch(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), m_state(Init)
 {
     ui.setupUi(this);
+
+    QTimer* createUpdateTimer = new QTimer(this);
+    connect(createUpdateTimer, SIGNAL(timeout()), this, SLOT(update_createProcess()));
+    createUpdateTimer->start();
 }
 
+void StationarySeedSearch::on_createButton_clicked() 
+{
+    const DBCreator::State threadState = m_dbc.getState();
 
-void StationarySeedSearch::on_createButton_clicked() {}
+    switch (m_state)
+    {
+    case StationarySeedSearch::Init:
+    {
+        m_dbc = DBCreator{};
+
+        std::thread th(std::ref(m_dbc));
+        th.detach();
+
+        m_state = Creating;
+        changeCreateButtonText(m_state);
+
+        break;
+    }
+    case StationarySeedSearch::Creating:
+        ui.createStatusLabel->setText("Aborting...");
+        m_dbc.abort();
+        m_state = Aborting;
+        break;
+    case StationarySeedSearch::Aborting:
+        //During aborting, button is unavailable.
+        break;
+    default:
+        break;
+    }
+
+}
 void StationarySeedSearch::on_sortButton_clicked(){}
+
+void StationarySeedSearch::update_createProcess() 
+{
+    switch (m_state)
+    {
+    case StationarySeedSearch::Init:
+        break;
+    case StationarySeedSearch::Creating:
+        ui.createStatusLabel->setText(QString::number(m_dbc.getProgress()*100.0)+"%...");
+
+        if (m_dbc.getState() == DBCreator::Finished) 
+        {
+            ui.createStatusLabel->setText("Completed.");
+            m_state = Init;
+            changeCreateButtonText(m_state);
+        }
+        else if (m_dbc.getState() == DBCreator::Error && !m_dbc.isAborted()) 
+        {
+            ui.createStatusLabel->setText(QString::fromStdString(m_dbc.getErrString()));
+
+            m_state = Init;
+            changeCreateButtonText(m_state);
+        }
+        break;
+    case StationarySeedSearch::Aborting:
+        if (m_dbc.isAborted()) 
+        {
+            ui.createStatusLabel->setText(QString::fromStdString(m_dbc.getErrString()));
+            m_state = Init;
+            changeCreateButtonText(m_state);
+        }
+        break;
+    default:
+        break;
+    }
+}
--- StationarySeedSearch/stationaryseedsearch.h
+++ StationarySeedSearch/stationaryseedsearch.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <QtWidgets/QWidget>
+#include<QTimer>
+#include<iostream>
 #include "ui_stationaryseedsearch.h"
+#include "../DB-CUI/db-create.hpp"
 
 class StationarySeedSearch : public QWidget
 {
@@ -12,8 +15,30 @@ public:
 
 private:
     Ui::StationarySeedSearchClass ui;
+    DBCreator m_dbc;
+    enum createState 
+    {
+        Init,
+        Creating,
+        Aborting,
+    } m_state;
+
+    void changeCreateButtonText(createState st)
+    {
+        switch (st)
+        {
+        case StationarySeedSearch::Creating: 
+        case StationarySeedSearch::Aborting:
+            ui.createButton->setText("Abort");
+            break;
+        default:
+            ui.createButton->setText("Create DB");
+            break;
+        }
+    }
 
 private slots:
     void on_createButton_clicked();
     void on_sortButton_clicked();
+    void update_createProcess();
 };
